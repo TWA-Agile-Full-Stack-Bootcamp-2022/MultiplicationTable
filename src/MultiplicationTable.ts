c11b1c19025d32b95996e1eeb96349a9bf3595c2
KUNWEI <kwei@thoughtworks.com>
2022-10-09 13:10:18 +0800

    feat: should return false when call isGreaterThanStartNumber given start number 3 and end number is 2

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,8 @@ export class MultiplicationTable {
   public render(start: number, end: number): string{
     return '1*1=1'
   }
+
+  isGreaterThanStartNumber(start: number, end: number) {
+    return false;
+  }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,5 +1,13 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
+// TASK 1 : should return false when call isGreaterThanStartNumber given start number 3 and end number is 2
+// TASK 2: should return true when call isGreaterThanStartNumber given start number 0 and end number is 2
+// TASK 3: should return false when call isInTheRange given start number 0 smaller than 1
+// TASK 4: should return false when call isInTheRange given end number 11
+// TASK 5: should return false when call isInTheRange given end number 10
+// TASK 6: should return true when call isInTheRange given end number 9 and start number 1
+// TASK 7: should return multiplication table when call render given start number 2 and end number 4
+
 describe('Multiplication Table', () => {
   it('should render multiplication table of (1, 1)', () => {
     // given
@@ -13,4 +21,19 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  it('should return false when call isGreaterThanStartNumber given start number 3 and end number is 2', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 3
+    const end = 2
+
+    //when
+    const isGreaterThanStartNumber = table.isGreaterThanStartNumber(start, end)
+
+    //then
+    expect(isGreaterThanStartNumber).toBeFalsy()
+  })
+
+
 })
