c14f698f45d0b003c463246b4c4abe0760b3e2bc
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-09 17:57:55 +0800

    refactor:  extract method checkNumberInRange and trim codes

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,14 +1,12 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    if (start>end){
+    if (start>end ||this.checkNumberInRangeOneToTen(start)||this.checkNumberInRangeOneToTen(end)){
       throw  new Error('error input')
     }
-    if (start<1 || start>10){
-      throw new Error('error input')
-    }
-    if (end<1 || end>10){
-      throw new Error('error input')
-    }
     return '1*1=1'
+  }
+
+  private checkNumberInRangeOneToTen(start: number) {
+    return start < 1 || start > 10
   }
 }
