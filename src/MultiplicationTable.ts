cc33b39913aa5c29b5fe7645022500ad71af42cd
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-09 17:51:05 +0800

    feat: add check start in range 1~10

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -3,6 +3,9 @@ export class MultiplicationTable {
     if (start>end){
       throw  new Error('error input')
     }
+    if (start<1 || start>10){
+      throw new Error('error input')
+    }
     return '1*1=1'
   }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -23,4 +23,10 @@ describe('Multiplication Table', () => {
     //then
     expect(() => table.render(start, end)).toThrow(new Error('error input'))
   })
+  it.each([{start: 0,end:1},{start: 11,end:13},{start:20,end:21}])('should throw error input exception when render given start not in range 1~10', (info) => {
+    //given
+    const table = new MultiplicationTable()
+    //then
+    expect(()=>table.render(info.start,info.end)).toThrow(new Error('error input'))
+  })
 })
