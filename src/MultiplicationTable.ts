9518e924e03a58b4f5cb2680b1490662b74f0fe5
Guangguang Ma <ggma@thoughtworks.com>
2022-10-09 16:16:05 +0800

    feat: render return empty string when end number not in range 1 to 10

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,14 +1,14 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    if (!this.checkStartNumberAndEndNumber(start)) {
-      return '';
+    if (!this.checkStartNumberAndEndNumber(start, end)) {
+      return ''
     }
 
     return '1*1=1'
   }
 
-  private checkStartNumberAndEndNumber(start: number) {
-    return start >= 1 && start <= 10;
+  private checkStartNumberAndEndNumber(start: number, end: number) {
+    return start >= 1 && start <= 10 && end >= 1 && end <= 10
   }
 
 }
