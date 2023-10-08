3e13361384f143f08f505df922e5c382a815fc9a
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-08 18:02:31 +0800

    feat: should return the result of start num is greater or not than the end number

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,8 @@ export class MultiplicationTable {
   public render(start: number, end: number): string{
     return '1*1=1'
   }
+
+  public isGreaterThan(start:number, end: number): boolean {
+    return start > end
+  }
 }
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,38 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  describe('should return the result of start num is greater or not than the end number', () => {
+    it('start number equals end number', () => {
+      // given
+      const table = new MultiplicationTable()
+      const start = 1
+      const end = 1
+      //when
+      const result = table.isGreaterThan(start, end)
+      //then
+      expect(result).toBe(false)
+    })
+    it('start number less than end number', () => {
+      // given
+      const table = new MultiplicationTable()
+      const start = 1
+      const end = 2
+      //when
+      const result = table.isGreaterThan(start, end)
+      //then
+      expect(result).toBe(false)
+    })
+
+    it('start number greater than end number', () => {
+      // given
+      const table = new MultiplicationTable()
+      const start = 2
+      const end = 1
+      //when
+      const result = table.isGreaterThan(start, end)
+      //then
+      expect(result).toBe(true)
+    })
+  })
 })
